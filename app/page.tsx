15fbb51bb9b37b6a8dcafddd7ec4e8d04baa11bf
Mário Ponte <ponteolavo30@gmail.com>
2026-01-16 19:28:23 +0000

    starting preparition for new database

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import CountdownCard from "./components/CountdownCard";
 import MiniCountdown from "./components/MiniCountdown";
 import { calculateCountdown, getNewYear, getNextChristmas } from "./counters";
+import { Button } from "@/components/ui/button";
+import { CalendarClock, MessageSquareWarning } from "lucide-react";
 
 export default function Home() {
 
@@ -32,6 +34,22 @@ export default function Home() {
 
   return (
     <section className="min-h-screen text-white px-4 py-10 max-w-screen-2xl m-auto">
+
+      <div className="mb-10 px-6 py-4 flex justify-between bg-slate-800 border border-slate-700 rounded-lg">
+        <div className="flex items-center gap-2">
+          <MessageSquareWarning size={40} />
+          <div className="flex flex-col">
+            <span className="font-semibold">Atention</span>
+            <span className="text-sm text-slate-200">This feature is still under construction, so it may contain problems or be temporarily unavailable.</span>
+          </div>
+        </div>
+        <div>
+          <Button variant={"secondary"} disabled className="font-semibold">
+            <CalendarClock />
+            <span>Create a Countdown</span>
+          </Button>
+        </div>
+      </div>
 
 
 
