64e2e6420cfeea892c73d4870a2c577e323171c2
Mário Ponte <ponteolavo30@gmail.com>
2026-02-23 19:59:12 +0000

    testing db

---
+++ script.ts
@@ -0,0 +1,30 @@
+import { prisma } from "./lib/prisma";
+
+async function main() {
+    // Create a new user with a post
+    const user = await prisma.countdown.create({
+        data: {
+            slug: "testcountdown",
+            name: "Test Countdown",
+            backgroundImg: "testcountdown.png",
+            eventDate: new Date("2027-01-01T00:00:00Z"),
+            eventRepeat: false,
+            eventType: "celebration"
+        }
+    });
+    console.log("Created user:", user);
+
+    // Fetch all countdowns setted to not repeat
+    const allCountdowns = await prisma.countdown.findMany({});
+    console.log("All Countdowns:", JSON.stringify(allCountdowns, null, 2));
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
